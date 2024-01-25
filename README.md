2be0423afdc6fe3eb0c38670fea7b6340866bbad
Guest_Admin <guest_admin@Ambers-MacBook-Pro.local>
2024-01-25 18:37:28 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Rock_Paper_Scissors_Python
+# Python_Game
+
+This project is a simple implementation of the classic rock-scissors-bag game in Python. In this game, the computer randomly selects one of the three options (rock, scissors, or bag), and the player also chooses one. The choices are then displayed simultaneously, and the winner is determined based on the following rules:
+
+Rock wins over scissors
+Scissors wins over bag
+Bag wins over rock
+If both the computer and the player choose the same option, the result is a tie.
+
+The player continues to play until either winning or losing to the computer.
---
+++ app.py
@@ -0,0 +1,47 @@
+import random
+
+while True:
+    choices = ["rock", "paper", "scissors"]
+
+    print("\nWelcome to the game: Rock, Paper, Scissors!\n")
+    print("Here are the rules of the game:")
+    print("""
+        Rock wins over scissors
+        Scissors wins over paper
+        Paper wins over rock
+        If both the computer and the player choose the same option, the result is a tie.")
+    """)
+
+    while True:
+        user_play = input("Please select (rock, paper, scissors): ").lower()
+
+
+        if user_play not in choices:
+            print("Invalid selection! Select (rock, paper, scissors)")
+            continue
+        else:
+            print(f"You selected: {user_play}")
+            break
+
+    computer_play = random.choice(choices)
+    print(f"The computer chose: {computer_play}")
+
+
+    if user_play == computer_play:
+        print("It's a tie!")
+    else:
+        if (
+            (user_play == 'rock' and computer_play == 'scissors') or
+            (user_play == 'paper' and computer_play == 'rock') or
+            (user_play == 'scissors' and computer_play == 'paper')
+        ):
+            print("Player wins!")
+        else:
+            print("Computer wins!")
+
+    while True:
+        play_again = input("\nWould you like to play again? Y/N: ").upper()
+
+        if play_again == 'N':
+            break
+     
